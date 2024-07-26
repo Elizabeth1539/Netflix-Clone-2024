00acde4b9713b240efb1342d8e0cc13cf70f738a
Elizabeth1539 <addiselizabeth12@gmail.com>
2024-07-25 20:57:57 -0400

    final

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "../../utiles/axios";
 import requests from "../../utiles/request";
 import "./banner.css";
+
 const Banner = () => {
 
 
